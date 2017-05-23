918059c012521732124f8835bdcf3e7ee1e06774
Mitch Crowe <crowe.mitch@gmail.com>
2017-05-23 10:01:41 -0700

    Release v0.0.6

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,6 @@
 import Router from './router';
 import Store from './store';
 declare var _default: {
-    StyleSheet: (sheet: object) => (...keys: any[]) => {};
     Router: typeof Router;
     Store: typeof Store;
     createApp: <T>(router: Router<T>, store: Store<T>) => void;
