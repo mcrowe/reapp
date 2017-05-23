6cb4cfcb55b5b94e5c1ab31745ab2627c045c1fb
Mitch Crowe <crowe.mitch@gmail.com>
2017-05-23 13:11:40 -0700

    Release v0.0.7

--- lib/router.d.ts
+++ lib/router.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="react" />
 import React = require('react');
-export declare type IGetProps<T> = (session: T) => object;
+export declare type IGetProps<T> = (params: object, session: T) => object;
 export declare type IComponent = React.ComponentClass<any>;
 export interface IHandler<T> {
     component: IComponent;
@@ -17,11 +17,17 @@ export default class Router<T> {
     handlers: IHandlerMap<T>;
     routes: IRoute[];
     constructor(initialRoute: IRoute);
-    route(path: string, component: IComponent, getProps?: IGetProps<T>): void;
+    route(path: string, component: IComponent, getProps?: IGetProps<T>): this;
     renderScene(session: T): React.ComponentElement<any, React.Component<any, React.ComponentState>>;
-    push(route: IRoute): void;
-    replace(route: IRoute): void;
+    push(path: string, params: object): void;
+    replace(path: string, params: object): void;
     pop(): void;
+    pusher(path: string, params: object): () => void;
+    popper(): () => void;
     getCurrentRoute(): IRoute;
+    makeRoute(path: string, params: object): {
+        path: string;
+        params: object;
+    };
     private resolve(route);
 }
