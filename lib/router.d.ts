0e7a1dced0c44552cb6b2b47f054e7e2b213cd01
Mitch Crowe <crowe.mitch@gmail.com>
2018-07-20 21:59:34 -0700

    Release v0.2.1

--- lib/router.d.ts
+++ lib/router.d.ts
@@ -11,8 +11,9 @@ export default class Router implements ISubscribable {
     constructor(initialRoute: IRoute);
     route(path: string, component: IComponent): this;
     renderScene(): React.ReactElement<any>;
-    push(path: string, params: object): void;
-    replace(path: string, params: object): void;
+    push(path: string, params?: object): void;
+    replace(path: string, params?: object): void;
+    go: (path: string, params?: object) => void;
     pop(): void;
     getCurrentRoute(): IRoute;
     subscribe: (fn: () => void) => () => void;
